b9cf41f536d102a5dd4d1a7ed27611798007c7c9
Pefruit03 <pegerken@gmail.com>
2023-07-10 14:39:13 +0700

    add models, fragment implementations

--- app/src/main/java/com/example/ouhotelbooking/MainActivity.java
+++ app/src/main/java/com/example/ouhotelbooking/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.ouhotelbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ouhotelbooking.R;
+import com.example.ouhotelbooking.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
         // Hello
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Button btnLogin = findViewById(R.id.button);
+        btnLogin.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+                if (fragment == null) {
+                    fragment = new LoginFragment();
+                    fragmentManager.beginTransaction()
+                            .add(R.id.fragment_container, fragment)
+                            .commit();
+                }
+            }
+        });
+
+
     }
 }
