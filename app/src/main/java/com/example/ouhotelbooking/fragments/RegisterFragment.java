f6ac9d52841b7d30cadfe509aa7ec69e66dfdd3e
Pefruit03 <pegerken@gmail.com>
2023-07-14 02:31:13 +0700

    change layouts

---
+++ app/src/main/java/com/example/ouhotelbooking/fragments/RegisterFragment.java
@@ -0,0 +1,20 @@
+package com.example.ouhotelbooking.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.ouhotelbooking.R;
+
+public class RegisterFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_register, container, false);
+    }
+}
