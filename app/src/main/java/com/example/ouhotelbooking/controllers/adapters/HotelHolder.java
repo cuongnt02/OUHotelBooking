ab9a81abf4aa2da04bb7194a8dd1925a2a0228be
cuongnt02 <cuong.ntc02@gmail.com>
2023-07-28 02:42:14 +0700

    Add some layouts

--- app/src/main/java/com/example/ouhotelbooking/controllers/adapters/HotelHolder.java
+++ app/src/main/java/com/example/ouhotelbooking/controllers/adapters/HotelHolder.java
@@ -1,5 +1,6 @@
 package com.example.ouhotelbooking.controllers.adapters;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -7,24 +8,32 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ouhotelbooking.R;
+import com.example.ouhotelbooking.controllers.HotelDetailActivity;
 import com.example.ouhotelbooking.data.model.Hotel;
 
-public class HotelHolder extends RecyclerView.ViewHolder {
+public class HotelHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    private TextView titleTextView;
-    private TextView addressTextView;
+        private TextView titleTextView;
+        private TextView addressTextView;
 
-    private Hotel hotel;
+        private Hotel hotel;
 
-    public HotelHolder(@NonNull View itemView) {
-        super(itemView);
-        titleTextView = (TextView) itemView.findViewById(R.id.layout_search_list_item_title);
-        addressTextView = (TextView) itemView.findViewById(R.id.layout_search_list_item_address);
+        public HotelHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            titleTextView = (TextView) itemView.findViewById(R.id.layout_search_list_item_title);
+            addressTextView = (TextView) itemView.findViewById(R.id.layout_search_list_item_address);
+        }
+
+        public void bindHotel(Hotel hotel) {
+            this.hotel = hotel;
+            titleTextView.setText(this.hotel.getName());
+            addressTextView.setText(this.hotel.getAddress());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = HotelDetailActivity.createIntent(view.getContext(), hotel.getId());
+            view.getContext().startActivity(intent);
+        }
     }
-
-    public void bindHotel(Hotel hotel) {
-        this.hotel = hotel;
-        titleTextView.setText(this.hotel.getName());
-        addressTextView.setText(this.hotel.getAddress());
-    }
-}
