0908ffbb9fee084cb02cfab249eedea9eb2ac724
cuongnt02 <cuong.ntc02@gmail.com>
2023-08-12 02:57:38 +0700

    add booking data to database

---
+++ app/src/main/java/com/example/ouhotelbooking/data/model/Booking.java
@@ -0,0 +1,51 @@
+package com.example.ouhotelbooking.data.model;
+
+import java.util.Date;
+
+public class Booking {
+    private int id;
+    private Date checkIn;
+    private Date checkOut;
+    private double totalPrice;
+    private int roomId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+}
---
+++ app/src/main/java/com/example/ouhotelbooking/data/schema/BookingDb.java
@@ -0,0 +1,10 @@
+package com.example.ouhotelbooking.data.schema;
+
+public class BookingDb {
+    public static final String TABLE_BOOKING = "booking";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_CHECK_IN = "check_in";
+    public static final String COLUMN_CHECK_OUT = "check_out";
+    public static final String COLUMN_TOTAL_PRICE = "total_price";
+    public static final String COLUMN_FK_ROOM = "room_id";
+}
